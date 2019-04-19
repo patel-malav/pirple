62df253a01a0c6d628b7679e14459823195479fe
malav patel <35185886+patel-malav@users.noreply.github.com>
2019-04-19 15:42:41 +0530

    Minor Corrections

--- Keeping Up With ES6/Assignment - 14/callback.js
+++ Keeping Up With ES6/Assignment - 14/callback.js
@@ -1,13 +1,24 @@
-let intro = 'Welcome to Ftp connect.', login = 'Remote adress to Ftp server : ', user = 'User : ',password = 'Password : ',
-    sucesses = 'Your\'e connection is established', failure = 'failed password authentication', greet = 'Welcome Mr. ';
+/* Author: Malav P. Patel
+ * GitHub: http://github.com/patel-malav
+ * Title: Callback Example
+ * Topic: Priple/Assignment - 14
+ */
+
+let intro = 'Welcome to Ftp connect.',
+    login = 'Remote adress to Ftp server : ',
+    user = 'User : ',
+    password = 'Password : ',
+    sucesses = 'Your\'e connection is established',
+    failure = 'failed password authentication',
+    greet = 'Welcome Mr. ';
 
 // authentication function.
 function fakeAuth(username, pwd, callBack) {
     setTimeout(() => {
-        if(username === 'malav', pwd === 'admin') {
+        if (username === 'malav', pwd === 'admin') {
             callBack(greet + username, 200);
         }
-    } , 3500);
+    }, 3500);
 }
 
 // print after a delay.
@@ -19,23 +30,22 @@ function print(str, time) {
 
 // fake input delay.
 function fakeInput(inputs, time, collected, callBack, callBack1) {
-    if(inputs.length === 0) {
+    if (inputs.length === 0) {
         console.log("Process Login.");
         callBack1(collected[1], collected[2], print);
-    }
-    else {
+    } else {
         setTimeout(() => {
             let temp = inputs.shift();
             console.log(temp);
             collected.push(temp.split(':')[1].replace(' ', ''));
-            callBack(inputs, time, collected,callBack, callBack1);
+            callBack(inputs, time, collected, callBack, callBack1);
         }, time);
     }
 }
 
 // login a user.
 function loginUser(input1, input2, input3, callBack1, callBack2) {
-    callBack1([login + input1, user + input2, password + input3] , 700, [], callBack1, callBack2);
+    callBack1([login + input1, user + input2, password + input3], 700, [], callBack1, callBack2);
 }
 
 // A function to login to a ftp simulation with fake input and fake authentication delay.
--- Keeping Up With ES6/Assignment - 14/promise.js
+++ Keeping Up With ES6/Assignment - 14/promise.js
@@ -1,21 +1,27 @@
+/* Author: Malav P. Patel
+ * GitHub: http://github.com/patel-malav
+ * Title: Callback Example
+ * Topic: Priple/Assignment - 14
+ */
+
 function loginUser(url, name, pwd) {
     let temp = fakeInput([url, name, pwd], [], 1200);
 
     temp.then((obj) => {
-        if(obj.inputs.length !== 0)
+        if (obj.inputs.length !== 0)
             return fakeInput(obj.inputs, obj.collected, 800);
         else
-            return fakeAuth(obj.collected[0],obj.collected[1],obj.collected[2]);
+            return fakeAuth(obj.collected[0], obj.collected[1], obj.collected[2]);
     }).then((obj) => {
-        if(obj.inputs.length !== 0)
+        if (obj.inputs.length !== 0)
             return fakeInput(obj.inputs, obj.collected, 1800);
         else
-            return fakeAuth(obj.collected[0],obj.collected[1],obj.collected[2]);
+            return fakeAuth(obj.collected[0], obj.collected[1], obj.collected[2]);
     }).then((obj) => {
-        if(obj.inputs.length !== 0)
+        if (obj.inputs.length !== 0)
             return fakeInput(obj.inputs, obj.collected, 800);
         else
-            return fakeAuth(obj.collected[0],obj.collected[1],obj.collected[2]);
+            return fakeAuth(obj.collected[0], obj.collected[1], obj.collected[2]);
     });
     return temp;
 }
@@ -24,30 +30,32 @@ function fakeAuth(url, name, pwd) {
     // console.log(url, name, pwd);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if(url && name === 'malav' && pwd === 'admin') {
+            if (url && name === 'malav' && pwd === 'admin') {
                 console.log(`Connection to ${url} OK\nWelcome ${name}\nPassword: ${pwd} OK.`);
                 resolve('OK');
-            }
-            else {
+            } else {
                 reject('wrong password');
             }
         }, 3500);
     });
 }
 
-function fakeInput(inputs, collected,time) {
+function fakeInput(inputs, collected, time) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             let temp = inputs.shift();
             console.log(temp);
             collected.push(temp);
-            resolve({inputs, collected});
+            resolve({
+                inputs,
+                collected
+            });
         }, time);
     });
 }
 
 console.log('Welcome to Ftp connect.');
 loginUser('www.filezilla.com', 'malav', 'admin').then((msg) => {
-    if(msg === 'OK')
+    if (msg === 'OK')
         console.log('Do FTP STUFF NOW\nAaaaaaaaaaaaaaaaaaa\nAhahaaahahahahhahahaha');
 }).catch((msg) => console.log(msg));
